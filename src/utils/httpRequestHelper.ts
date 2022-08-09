eb39a4157f6ee9e9537523bbda0d8ef43a6568f5
veseah <veseah@microsoft.com>
2022-08-08 19:45:57 -0700

    code cleanup

--- src/utils/httpRequestHelper.ts
+++ src/utils/httpRequestHelper.ts
@@ -1,6 +1,4 @@
-import { Logger } from "./logger";
-import * as https from "https";
-import * as http from "http";
+import { RequestUtils } from "./requestUtils";
 
 export class HttpRequestHelper {
     static async makeGETRequest(
@@ -10,10 +8,7 @@ export class HttpRequestHelper {
         parameters?: Map<string, string | number>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ): Promise<any> {
-        return this.makeRequest(
-            name,
-            this.createOptions(url, "GET", headers, parameters)
-        );
+        return RequestUtils.sendRequest(name, RequestUtils.createOptions(url, "GET", headers, parameters));
     }
     static async makeDELETERequest(
         name: string,
@@ -22,7 +17,7 @@ export class HttpRequestHelper {
         parameters?: Map<string, string | number>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ): Promise<any> {
-        return this.makeRequest(name, this.createOptions(url, "DELETE", headers, parameters));
+        return RequestUtils.sendRequest(name, RequestUtils.createOptions(url, "DELETE", headers, parameters));
     }
     static async makePUTRequest(
         name: string,
@@ -32,7 +27,7 @@ export class HttpRequestHelper {
         parameters?: Map<string, string | number>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ): Promise<any> {
-        return this.makePostPutRequest(name, "PUT", url, payload, headers, parameters);
+        return RequestUtils.sendRequest(name, RequestUtils.createOptions(url, "PUT", headers, parameters, payload), payload);
     }
     static async makePOSTRequest(
         name: string,
@@ -42,94 +37,6 @@ export class HttpRequestHelper {
         parameters?: Map<string, string | number>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ): Promise<any> {
-        return this.makePostPutRequest(name, "POST", url, payload, headers, parameters);
-    }
-    private static async makePostPutRequest(
-        name: string,
-        method: string,
-        url: URL,
-        payload: string | undefined,
-        headers?: Map<string, string | number>,
-        parameters?: Map<string, string | number>
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ): Promise<any> {
-        if (payload) {
-            headers = headers || new Map<string, string>();
-            headers.set("Content-Type", "application/json");
-            headers.set("Content-Length", payload.length);
-        }
-        return this.makeRequest(name, this.createOptions(url, method, headers, parameters), payload);
-    }
-    private static async makeRequest(
-        name: string,
-        options: https.RequestOptions,
-        payload?: string | undefined
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ): Promise<any> {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return new Promise<any>((resolve, reject) => {
-            const req = https.request(options, (res) => {
-                const statusCode: number = this.handleResponse(res) || 400;
-
-                if (statusCode > 299) {
-                    reject(`problem with request, status code: ${statusCode}`);
-                }
-
-                let body = "";
-                res.on("data", (chunk) => {
-                    body += chunk;
-                });
-
-                res.on("end", () => {
-                    const contentTypes = (res.headers["content-type"] || "")
-                        .split(";")
-                        .map((s) => s.toLowerCase());
-
-                    resolve(
-                        contentTypes.indexOf("application/json") !== -1
-                            ? JSON.parse(body)
-                            : body
-                    );
-                });
-            });
-            req.on("error", (e) => {
-                reject(`problem with request, ${name}: ${e.message}`);
-            });
-
-            if (payload) {
-                req.write(payload);
-            }
-            req.end();
-        });
-    }
-    private static handleResponse(res: http.IncomingMessage): number | undefined {
-        Logger.debug(`STATUS: ${res.statusCode}`);
-        Logger.debug(`HEADERS: ${JSON.stringify(res.headers)}`);
-        res.setEncoding("utf8");
-        return res.statusCode;
-    }
-    private static createOptions(
-        url: URL,
-        method: string,
-        headers?: Map<string, string | number>,
-        parameters?: Map<string, string | number>
-    ): https.RequestOptions {
-        const options = {
-            hostname: url.hostname,
-            port: url.port,
-            path: url.pathname,
-            method: method,
-            rejectUnauthorized: false,
-            headers: headers ? Object.fromEntries(headers) : {},
-        } as https.RequestOptions;
-
-        if (parameters) {
-            const qParams = [...parameters.keys()].map(
-                (k) => `${k}=${encodeURIComponent(parameters.get(k) as string)}`
-            );
-            options.path += "?" + qParams.join("&");
-        }
-
-        return options;
+        return RequestUtils.sendRequest(name, RequestUtils.createOptions(url, "POST", headers, parameters, payload), payload);
     }
 }
---
+++ src/utils/requestUtils.ts
@@ -0,0 +1,80 @@
+import { Logger } from "./logger";
+import * as https from "https";
+import * as http from "http";
+
+export class RequestUtils {
+    static async sendRequest(
+        name: string,
+        options: https.RequestOptions,
+        payload?: string | undefined
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ): Promise<any> {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        return new Promise<any>((resolve, reject) => {
+            const req = https.request(options, (res) => {
+                const statusCode: number = this.handleResponse(res) || 400;
+
+                if (statusCode > 299) {
+                    reject(`problem with request, status code: ${statusCode}`);
+                }
+
+                let body = "";
+                res.on("data", (chunk) => {
+                    body += chunk;
+                });
+
+                res.on("end", () => {
+                    resolve(this.isJSONBody(res) ? JSON.parse(body) : body);
+                });
+            });
+            req.on("error", (e) => {
+                reject(`problem with request, ${name}: ${e.message}`);
+            });
+
+            if (payload) {
+                req.write(payload);
+            }
+            req.end();
+        });
+    }
+    static createOptions(
+        url: URL,
+        method: string,
+        headers?: Map<string, string | number>,
+        parameters?: Map<string, string | number>,
+        payload?: string | undefined
+    ): https.RequestOptions {
+        if (payload) {
+            headers = headers || new Map<string, string>();
+            headers.set("Content-Type", "application/json");
+            headers.set("Content-Length", payload.length);
+        }
+        const options = {
+            hostname: url.hostname,
+            port: url.port,
+            path: url.pathname,
+            method: method,
+            rejectUnauthorized: false,
+            headers: headers ? Object.fromEntries(headers) : {},
+        } as https.RequestOptions;
+
+        if (parameters) {
+            const qParams = [...parameters.keys()].map(
+                (k) => `${k}=${encodeURIComponent(parameters.get(k) as string)}`
+            );
+            options.path += "?" + qParams.join("&");
+        }
+
+        return options;
+    }
+    private static handleResponse(res: http.IncomingMessage): number | undefined {
+        Logger.debug(`STATUS: ${res.statusCode}`);
+        Logger.debug(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding("utf8");
+        return res.statusCode;
+    }
+    private static isJSONBody(res: http.IncomingMessage): boolean {
+        const contentTypes = (res.headers["content-type"] || "").split(";").map((s) => s.toLowerCase());
+        return (contentTypes.indexOf("application/json") !== -1);
+    }
+}
